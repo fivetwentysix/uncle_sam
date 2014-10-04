386bc88c2fff889795174d6e5179680b005f2a11
Patrick Ma <fivetwentysix@gmail.com>
2014-10-04 12:52:56 -0700

    add exception handling to filing_status input

--- spec/uncle_sam/calculator_spec.rb
+++ spec/uncle_sam/calculator_spec.rb
@@ -9,16 +9,26 @@ module UncleSam
     :qualifying_surviving_spouse  => 12400
   }
 
+  UnknownFilingStatusError = Class.new(Exception)
+
   class Calculator
     attr_reader :remaining_income, :filing_status
 
     def initialize(net_income, filing_status)
       @remaining_income = net_income
       @filing_status    = filing_status
+
+      raise UnknownFilingStatusError if filing_status_is_invalid?
     end
 
     def make_standard_deductions
       @remaining_income -= FILING_STATUS_OPTIONS[filing_status]
+    end
+
+    private
+
+    def filing_status_is_invalid?
+      FILING_STATUS_OPTIONS[filing_status].nil?
     end
   end
 end
@@ -32,7 +42,11 @@ describe UncleSam::Calculator do
       calculator.make_standard_deductions
       expect(calculator.remaining_income).to eq(average_net_income - UncleSam::FILING_STATUS_OPTIONS[:single])
     end
+  end
 
-    it 'only accepts values matching keys contained in UncleSam::FILING_STATUS_OPTIONS'
+  it 'rejects unknown filing status values' do
+    expect do
+      UncleSam::Calculator.new(average_net_income, :invalid_value)
+    end.to raise_error(UncleSam::UnknownFilingStatusError)
   end
 end
